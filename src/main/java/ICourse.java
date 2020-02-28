d87ae7046a3eb18302d5f8f86163420c98465a0d
wanghaiyang1 <wanghaiyang1@xiaomi.com>
2020-02-28 18:26:47 +0800

    开闭2

---
+++ src/main/java/ICourse.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+
+/**
+ * @Auther: wanghaiyang
+ * @Date: 20/2/25 15:42
+ * @Description:
+ */
+public interface ICourse {
+
+    Integer getId();
+    String getName();
+    BigDecimal getPrice();
+
+}
